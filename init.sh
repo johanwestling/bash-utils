760fd191c3d59ceae8a7d5f63dbb6c814b9aab6a
Johan Westling <johan.westling@paf.com>
2020-06-04 16:47:42 +0300

    Added auto update

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@
 return_path="$PWD"
 
 # Clone repository (if missing)
+[ -f "bash-utils/init.sh" ] cd "bash-utils" && git pull
 [ -f "bash-utils/init.sh" ] || git clone "git@github.com:johanwestling/bash-utils.git" "bash-utils"
 
 # Load all util files.
