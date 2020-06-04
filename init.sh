a6082581dffb01af94fc241c1e5321dc0becb97a
Johan Westling <johan.westling@paf.com>
2020-06-04 16:55:31 +0300

    Suppres git output

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ return_path="$PWD"
 
 # Clone repository (if missing)
 ! [ -f "bash-utils/init.sh" ] || (cd "bash-utils" && git pull --quiet)
-[ -f "bash-utils/init.sh" ] || (git clone --quiet "git@github.com:johanwestling/bash-utils.git" "bash-utils")
+[ -f "bash-utils/init.sh" ] || git clone --quiet "git@github.com:johanwestling/bash-utils.git" "bash-utils"
 
 # Load all util files.
 source <(cat "bash-utils/utils/"*)
