99dc31294d53bb1e178dfb6a4db32ce84a32560e
Johan Westling <johan.westling@paf.com>
2020-06-05 07:29:11 +0300

    Removed git pull from init

--- init.sh
+++ init.sh
@@ -4,10 +4,6 @@ return_path="$PWD"
 
 if ! [ -f "bash-utils/init.sh" ]; then
   git clone -q "git@github.com:johanwestling/bash-utils.git" "bash-utils"
-elif [ -f "bash-utils/init.sh" ]; then
-  cd "bash-utils"
-  git pull -q
-  cd "$return_path"
 fi
 
 # Load all util files.
